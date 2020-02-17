58f4de5cf1e06547fe165987a167336e1665673b
wuchunlong0 <wcl6005@163.com>
2020-02-17 08:25:57 +0800

    admin 不是显示图片url路径，直接显示图片

--- mysite/account/admin.py
+++ mysite/account/admin.py
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
 from django.contrib import admin
-
 from .models import  Image
+
+
 
 @admin.register(Image)
 class ImageAdmin(admin.ModelAdmin):    
-    list_display = ('id','pic_name','pic_path')
+    list_display = ('id','pic_name','pic_path', 'image_data')
+    readonly_fields = ('image_data',)
    
+  
   
--- mysite/account/models.py
+++ mysite/account/models.py
@@ -5,11 +5,21 @@ from django.db import models
 from django.db.models.signals import post_delete
 from django.dispatch import receiver 
 from django.conf import settings
+from django.utils.html import format_html
+
     
 # 上传图片  设置media以显示图片
 class Image(models.Model):
-    pic_name = models.CharField('图片名称', max_length=40)
-    pic_path = models.ImageField('选择图片', upload_to="pic_folder/", default='')
+    pic_name = models.CharField('名称', max_length=40)
+    pic_path = models.ImageField('图片 url', upload_to="pic_folder/", default='')
+
+    def image_data(self):
+        """ admin 不是显示图片url路径，直接显示图片   """
+        return format_html(
+            '<img src="{}" width="100px"/>',
+            self.pic_path.url,
+        )
+    image_data.short_description = u'图片'
     
     
 @receiver(post_delete, sender=Image)
