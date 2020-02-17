1c059bf693a8cebe022ad4524c3a12860df6b1aa
wuchunlong0 <wcl6005@163.com>
2020-02-17 08:06:51 +0800

    admin 删除数据库记录，同时也删除图像文件

--- mysite/account/models.py
+++ mysite/account/models.py
@@ -1,7 +1,25 @@
 # -*- coding: utf-8 -*-
+
+import os
 from django.db import models
+from django.db.models.signals import post_delete
+from django.dispatch import receiver 
+from django.conf import settings
     
 # 上传图片  设置media以显示图片
 class Image(models.Model):
     pic_name = models.CharField('图片名称', max_length=40)
     pic_path = models.ImageField('选择图片', upload_to="pic_folder/", default='')
+    
+    
+@receiver(post_delete, sender=Image)
+def delete_upload_files(sender, instance, **kwargs):   
+    """
+    admin 删除数据库记录，同时也删除图像文件
+    """
+    files = getattr(instance, 'pic_path', '')
+    if not files:
+        return 
+    fname = os.path.join(settings.MEDIA_ROOT, str(files))
+    if os.path.isfile(fname):
+        os.remove(fname)
--- mysite/account/urls.py
+++ mysite/account/urls.py
@@ -2,8 +2,7 @@
 from django.conf.urls import url, include
 from . import views
 
-urlpatterns = [ 
-    url(r'index/$', views.index, name='index'),      
+urlpatterns = [    
     url(r'upload/pic/(?P<page>\d*)?$', views.upload_pic, name='upload_pic'),
     url(r'', views.index, name='index'),   #必须放在最后
 ]
